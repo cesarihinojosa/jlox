d1dee184cf3463a58d87ea7d3648ebee353c91f4
Cesar Hinojosa <cesarivanhinojosa@gmail.com>
2026-03-02 20:55:20 -0600

    Scanner

--- src/main/java/com/cesar/Lox.java
+++ src/main/java/com/cesar/Lox.java
@@ -1,16 +1,63 @@
 package com.cesar;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.Buffer;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.Scanner;
 
 public class Lox {
+    static boolean hadError = false;
     public static void main(String[] args) throws IOException{
         if (args.length > 1){
             System.out.print("Usage: jlox [script]");
             System.exit(64);
         } else if (args.length == 1) {
-            //runFile(args[0]);
+            runFile(args[0]);
         } else{
-            //runPrompt();
+            runPrompt();
         }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(path));
+        run(new String(bytes, Charset.defaultCharset()));
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;){ // why this instead of a while?
+            System.out.print("> ");
+            String line = reader.readLine();
+            if (line == null) break;
+            run(line);
+            hadError = false; // reset after each line so the whole program doesnt die
+        }
+    }
+
+    private static void run(String source){
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+        for (Token token : tokens){
+            System.out.print(token);
+        }
+    }
+
+    static void error(int line, String message){
+        report(line, message, message);
+    }
+
+    // this is bare minimum and can be improved upon later
+    private static void report(int line, String where, String message){
+        System.err.println("[line" + line + " ] Error" + where + ": " + message);
+        hadError = true; // if this is true, we shouldnt execute code that we know contains an error
     }
 }
---
+++ src/main/java/com/cesar/Scanner.java
@@ -0,0 +1,63 @@
+package com.cesar;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.cesar.TokenType.*;
+
+public class Scanner {
+    private final String source; // raw source code is just a string
+    private final List<Token> tokens = new ArrayList<>();
+    private int start = 0; // start of the lexeme
+    private int current = 0; // current char being processed. like 2 pointer ^
+    private int line = 1;
+
+    Scanner(String source){
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+        while (!isAtEnd()){
+            start = current;
+            scanToken();
+        }
+        tokens.add(new Token(EOF, "", null, line));
+        return tokens;
+    }
+
+    private void scanToken(){
+        char c = advance();
+        switch (c) {
+            case '(': addToken(LEFT_PAREN); break;
+            case ')': addToken(RIGHT_PAREN); break;
+            case '{': addToken(LEFT_BRACE); break;
+            case '}': addToken(RIGHT_BRACE); break;
+            case ',': addToken(COMMA); break;
+            case '.': addToken(DOT); break;
+            case '-': addToken(MINUS); break;
+            case '+': addToken(PLUS); break;
+            case ';': addToken(SEMICOLON); break;
+            case '*': addToken(STAR); break;
+            default:
+                Lox.error(line, "Unexpected character.");
+                break;
+        }
+    }
+
+    private boolean isAtEnd(){
+        return current >= source.length();
+    }
+
+    private char advance() {
+        return source.charAt(current++);
+    }
+
+    private void addToken(TokenType type){
+        addToken(type, null);
+    }
+
+    private void addToken(TokenType type, Object literal){
+        String text = source.substring(start, current);
+        tokens.add(new Token(type, text, literal, line));
+    }
+}
