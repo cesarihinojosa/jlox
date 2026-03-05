24e26e27f8a1e1bcf8ea4b48fa45d05a9fdcfd8f
Cesar Hinojosa <cesarivanhinojosa@gmail.com>
2026-03-04 20:10:03 -0600

    adding single and double chracter lexems, including comments; 4.6.1

--- src/main/java/com/cesar/Scanner.java
+++ src/main/java/com/cesar/Scanner.java
@@ -38,6 +38,32 @@ public class Scanner {
             case '+': addToken(PLUS); break;
             case ';': addToken(SEMICOLON); break;
             case '*': addToken(STAR); break;
+            case '!':
+                addToken(match('=') ? BANG_EQUAL : BANG);
+                break;
+            case '=':
+                addToken(match('=') ? EQUAL_EQUAL: EQUAL);
+                break;
+            case '<':
+                addToken(match('=') ? LESS_EQUAL : LESS);
+                break;
+            case '>':
+                addToken(match('=') ? GREATER_EQUAL : EQUAL);
+                break;
+            case '/':
+                if (match('/')){
+                    while (peek() != '\n' && !isAtEnd()) advance();
+                } else {
+                    addToken(SLASH);
+                }
+                break;
+            case ' ':
+            case '\r':
+            case '\t':
+                break;
+            case '\n':
+                line++;
+                break;
             default:
                 Lox.error(line, "Unexpected character.");
                 break;
@@ -59,5 +85,17 @@ public class Scanner {
     private void addToken(TokenType type, Object literal){
         String text = source.substring(start, current);
         tokens.add(new Token(type, text, literal, line));
+    }
+
+    private boolean match(char excpected){
+        if (isAtEnd()) return false;
+        if (source.charAt(current) != excpected) return false;
+        current++; //otherwise
+        return true;
+    }
+
+    private char peek() {
+        if (isAtEnd()) return '\0';
+        return source.charAt(current);
     }
 }
